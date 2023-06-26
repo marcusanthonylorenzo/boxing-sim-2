05b3105fbe80e0e36d52d4ddbbe889655894d131
Marcus Lorenzo <marcus.lorenzo@eyecuelab.com>
2023-06-26 13:10:58 -0700

    add AcceptFightModal buttons

--- components/modals/FightAcceptModal.tsx
+++ components/modals/FightAcceptModal.tsx
@@ -12,6 +12,9 @@ const FightAcceptModal = ({
     // useEffect(() => console.log(hideModal), [hideModal])
 
     const handleHideShow = () => hideModal === 'hidden' ? setHideModal(``) : setHideModal(`hidden`)
+    const handleAcceptFight = () => {
+        console.log(`test accepted!`)
+    }
 
   return (
     <div className={`${hideModal} overflow-hidden h-full w-full`}>
@@ -22,8 +25,22 @@ const FightAcceptModal = ({
             onClick={() => handleHideShow() }>
             
             <div id={`FightAcceptModal-wrapper`}
-                className={`flex absolute top-[0] bg-zinc-100 h-[80%] w-[80%] max-w-2xl rounded-md shadow-2xl items-center justify-center overflow-hidden`}>
+                className={`flex flex-col absolute top-[0] bg-zinc-100 h-[80%] w-[80%] max-w-2xl rounded-md shadow-2xl items-center justify-center overflow-hidden`}>
                 <h2 className={`relative font-semibold text-[#705e93] text-[32px]`}>SIGN CONTRACT</h2>
+
+                <button id={`FightAcceptModal-accept_fight`}
+                    className={`relative top-[5%] w-[100px] h-[50px] bg-black text-white rounded-md`}
+                    onClick={() => handleAcceptFight()}
+                >
+                    <h5>FIGHT</h5>
+                </button>
+
+                <button id={`FightAcceptModal-reject_fight`}
+                    className={`relative top-[7%] w-[100px] h-[50px] bg-zinc-400 opacity-50 text-white rounded-md`}
+                    onClick={() => console.log(`fight rejected`)}
+                >
+                    <h5>Cancel</h5>
+                </button>
             </div>
         </div>
 
